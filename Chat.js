b2dc25d3453656f519c750f08d9ba2a75e4df22d
CABARCAS-Q <150747859+CABARCAS-Q@users.noreply.github.com>
2023-11-13 19:04:14 -0500

    SCRIP

---
+++ Chat.js
@@ -0,0 +1,82 @@
+﻿var you = {};
+you.avatar = "../Imagenes/operador.png";
+
+var me = {};
+me.avatar = "../Imagenes/logo48.png";
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+//-- No use time. It is a javaScript effect.
+function insertChat(who, text, time) {
+    if (time === undefined) {
+        time = 0;
+    }
+    var control = "";
+    var date = formatAMPM(new Date());
+
+    if (who == "me") {
+        control = '<li style="width:100%">' +
+                        '<div class="msj macro">' +
+                        '<div class="avatar"><img class="img-circle" style="width:100%;" src="' + me.avatar + '" /></div>' +
+                            '<div class="text text-l">' +
+                                '<p>' + text + '</p>' +
+                                '<p><small>' + date + '</small></p>' +
+                            '</div>' +
+                        '</div>' +
+                    '</li>';
+    } else {
+        control = '<li style="width:100%;">' +
+                        '<div class="msj-rta macro">' +
+                            '<div class="text text-r">' +
+                                '<p>' + text + '</p>' +
+                                '<p><small>' + date + '</small></p>' +
+                            '</div>' +
+                        '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:100%;" src="' + you.avatar + '" /></div>' +
+                  '</li>';
+    }
+    setTimeout(
+        function () {
+            $("#ul2").append(control).scrollTop($("#ul2").prop('scrollHeight'));
+        }, time);
+
+}
+
+$(".mensajen").on("keydown", function (e) {
+    if (e.which == 13) {
+        var text = $(this).val();
+        if (text !== "") {
+            insertChat("me", text);
+            $(this).val('');
+        }
+    }
+});
+
+$('body > div > div > div:nth-child(2) > span').click(function () {
+    $(".mensajen").trigger({ type: 'keydown', which: 13, keyCode: 13 });
+})
+
+//-- Clear Chat
+//resetChat();
+function enviar_chat() {
+
+    var mensaje = document.getElementById('mensajen').value;
+    insertChat("you", mensaje, 0);
+    document.getElementById('mensajen').value = '';
+}
+//-- Print Messages
+
+insertChat("me", "Hello Tom...", 10);
+insertChat("you", "Hello Tom...", 100);
+
+
+
+//-- NOTE: No use time on insertChat.
